fe9b4a40bf8471d22aa956c89b6108f4fc181d91
JATG <jatg81@gmail.com>
2019-12-27 22:12:19 -0500

    Update RFM69Pi.ino

--- firmware/RFM69Pi.ino
+++ firmware/RFM69Pi.ino
@@ -1,17 +1,17 @@
-// Adding frequency features, author JohnO, 2013-09-05
-// Major EEPROM format change, refactoring, and cleanup for v12, 2014-02-13
+//RFM69Pi V2 with RFM69CW Firmware
+//Based on JCW RF12 Demo: https://github.com/jcw/jeelib/tree/master/examples/RF12/RF12demo
+//Edited for RFM69Pi and emonPi operation December 2019 by JATG
+//--------------------------------------------------------------------------------------------------------------------------------------------
 
-#define RF69_COMPAT 1 // define this to use the RF69 driver i.s.o. RF12
-
-#include <JeeLib.h>
+#include <RF69.h>
 #include <util/crc16.h>
 #include <avr/eeprom.h>
 #include <avr/pgmspace.h>
 #include <util/parity.h>
 
-#define MAJOR_VERSION RF12_EEPROM_VERSION // bump when EEPROM layout changes
+#define MAJOR_VERSION RF69_EEPROM_VERSION // bump when EEPROM layout changes
 #define MINOR_VERSION 2                   // bump on other non-trivial changes
-#define VERSION "[RF12demo.14]"           // keep in sync with the above
+#define VERSION "[RF69demo.20]"           // keep in sync with the above
 
 #define SERIAL_BAUD 38400   // max baud for 8Mhz RFM12Pi http://openenergymonitor.org/emon/node/6244/
 #define DATAFLASH   0       // set to 0 for non-JeeLinks, else 4/8/16 (Mbit)
@@ -22,9 +22,7 @@
 const char INVALID1[] PROGMEM = "\rInvalid\n";
 const char INITFAIL[] PROGMEM = "config save failed\n";
 
-#if RF69_COMPAT
-  byte trace_mode = 0;
-#endif
+byte trace_mode = 0;
 
 #define _receivePin 8
 static int _bitDelay;
@@ -77,7 +75,6 @@ static byte inChar () {
 
 
 static unsigned long now () {
-  // FIXME 49-day overflow
   return millis() / 1000;
 }
 
@@ -93,32 +90,30 @@ static void printOneChar (char c) {
 }
 
 /// @details
-/// For the EEPROM layout, see http://jeelabs.net/projects/jeelib/wiki/RF12demo
+/// For the EEPROM layout, see http://jeelabs.net/projects/jeelib/wiki/RF69demo
 /// Useful url: http://blog.strobotics.com.au/2009/07/27/rfm12-tutorial-part-3a/
 
-// RF12 configuration area
+// RF69 configuration area
 typedef struct {
-  byte nodeId;            // used by rf12_config, offset 0
-  byte group;             // used by rf12_config, offset 1
-  byte format;            // used by rf12_config, offset 2
-byte hex_output   :
-  2;   // 0 = dec, 1 = hex, 2 = hex+ascii
-byte collect_mode :
-  1;   // 0 = ack, 1 = don't send acks
-byte quiet_mode   :
-  1;   // 0 = show all, 1 = show only valid packets
-byte spare_flags  :
-  4;
-  word frequency_offset;  // used by rf12_config, offset 4
-  byte pad[RF12_EEPROM_SIZE-8];
+  byte nodeId;            // used by rf69_config, offset 0
+  byte group;             // used by rf69_config, offset 1
+  byte format;            // used by rf69_config, offset 2
+  byte hex_output   : 2;   // 0 = dec, 1 = hex, 2 = hex+ascii
+  byte collect_mode : 1;   // 0 = ack, 1 = don't send acks
+  byte quiet_mode   : 1;   // 0 = show all, 1 = show only valid packets
+  byte led_enable   : 1;    // enable led activity
+  byte spare_flags  : 3;
+  word frequency_offset;  // used by rf69_config, offset 4
+  byte pad[RF69_EEPROM_SIZE-8];
   word crc;
+ 
 }
-RF12Config;
+RF69Config;
 
-static RF12Config config;
+static RF69Config config;
 static char cmd;
 static word value;
-static byte stack[RF12_MAXDATA+4], top, sendLen, dest;
+static byte stack[RF69_MAXDATA+4], top, sendLen, dest;
 static byte testCounter;
 
 static void showNibble (byte nibble) {
@@ -156,93 +151,31 @@ static word calcCrc (const void* ptr, byte len) {
 }
 
 static void loadConfig () {
-  // eeprom_read_block(&config, RF12_EEPROM_ADDR, sizeof config);
+  // eeprom_read_block(&config, RF69_EEPROM_ADDR, sizeof config);
   // this uses 166 bytes less flash than eeprom_read_block(), no idea why
   for (byte i = 0; i < sizeof config; ++ i)
-    ((byte*) &config)[i] = eeprom_read_byte(RF12_EEPROM_ADDR + i);
+    ((byte*) &config)[i] = eeprom_read_byte(RF69_EEPROM_ADDR + i);
 }
 
 static void saveConfig () {
   config.format = MAJOR_VERSION;
   config.crc = calcCrc(&config, sizeof config - 2);
-  // eeprom_write_block(&config, RF12_EEPROM_ADDR, sizeof config);
+  // eeprom_write_block(&config, RF69_EEPROM_ADDR, sizeof config);
   // this uses 170 bytes less flash than eeprom_write_block(), no idea why
-  eeprom_write_byte(RF12_EEPROM_ADDR, ((byte*) &config)[0]);
+  eeprom_write_byte(RF69_EEPROM_ADDR, ((byte*) &config)[0]);
   for (byte i = 0; i < sizeof config; ++ i)
-    eeprom_write_byte(RF12_EEPROM_ADDR + i, ((byte*) &config)[i]);
+    eeprom_write_byte(RF69_EEPROM_ADDR + i, ((byte*) &config)[i]);
 
-  if (rf12_configSilent())
-    rf12_configDump();
+  if (rf69_configSilent())
+    rf69_configDump();
   else
     showString(INITFAIL);
 }
 
 static byte bandToFreq (byte band) {
-  return band == 4 ? RF12_433MHZ : band == 8 ? RF12_868MHZ : band == 9 ? RF12_915MHZ : 0;
+  return band == 4 ? RF69_433MHZ : band == 8 ? RF69_868MHZ : band == 9 ? RF69_915MHZ : 0;
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// OOK transmit code
-
-#if RF69_COMPAT // not implemented in RF69 compatibility mode
-static void fs20cmd(word house, byte addr, byte cmd) {
-}
-static void kakuSend(char addr, byte device, byte on) {
-}
-#else
-
-// Turn transmitter on or off, but also apply asymmetric correction and account
-// for 25 us SPI overhead to end up with the proper on-the-air pulse widths.
-// With thanks to JGJ Veken for his help in getting these values right.
-static void ookPulse(int on, int off) {
-  rf12_onOff(1);
-  delayMicroseconds(on + 150);
-  rf12_onOff(0);
-  delayMicroseconds(off - 200);
-}
-
-
-static void fs20sendBits(word data, byte bits) {
-  if (bits == 8) {
-    ++bits;
-    data = (data << 1) | parity_even_bit(data);
-  }
-  for (word mask = bit(bits-1); mask != 0; mask >>= 1) {
-    int width = data & mask ? 600 : 400;
-    ookPulse(width, width);
-  }
-}
-
-static void fs20cmd(word house, byte addr, byte cmd) {
-  byte sum = 6 + (house >> 8) + house + addr + cmd;
-  for (byte i = 0; i < 3; ++i) {
-    fs20sendBits(1, 13);
-    fs20sendBits(house >> 8, 8);
-    fs20sendBits(house, 8);
-    fs20sendBits(addr, 8);
-    fs20sendBits(cmd, 8);
-    fs20sendBits(sum, 8);
-    fs20sendBits(0, 1);
-    delay(10);
-  }
-}
-
-static void kakuSend(char addr, byte device, byte on) {
-  int cmd = 0x600 | ((device - 1) << 4) | ((addr - 1) & 0xF);
-  if (on)
-    cmd |= 0x800;
-  for (byte i = 0; i < 4; ++i) {
-    for (byte bit = 0; bit < 12; ++bit) {
-      ookPulse(375, 1125);
-      int on = bitRead(cmd, bit) ? 1125 : 375;
-      ookPulse(on, 1500 - on);
-    }
-    ookPulse(375, 375);
-    delay(11); // approximate
-  }
-}
-
-#endif
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // DataFlash code
@@ -277,14 +210,10 @@ const char helpText1[] PROGMEM =
 "  ...,<nn> s - send data packet to node <nn>, no ack\n"
 "  <n> q      - set quiet mode (1 = don't report bad packets)\n"
 "  <n> x      - set reporting format (0: decimal, 1: hex, 2: hex+ascii)\n"
-#if RF69_COMPAT
 "  <nnn> y    - enable signal strength trace mode, default:0 (disabled)\n"
 "               sample interval <nnn> secs/100 (0.01s-2.5s) eg 10y=0.1s\n"
-#endif
 "  123 z      - total power down, needs a reset to start up again\n"
-"Remote control commands:\n"
-"  <hchi>,<hclo>,<addr>,<cmd> f     - FS20 command (868 MHz)\n"
-"  <addr>,<dev>,<on> k              - KAKU command (433 MHz)\n"
+"  <n> l      - enable activity led (0 = disable led)\n"
 ;
 
 const char helpText2[] PROGMEM =
@@ -302,7 +231,7 @@ static void showHelp () {
   if (df_present())
     showString(helpText2);
   showString(PSTR("Current configuration:\n"));
-  rf12_configDump();
+  rf69_configDump();
 }
 
 static void handleInput (char c) {
@@ -334,11 +263,11 @@ static void handleInput (char c) {
     // input: band,group,node,header,data...
     stack[top++] = value;
     // TODO: frequency offset is taken from global config, is that ok?
-    rf12_initialize(stack[2], bandToFreq(stack[0]), stack[1],
+    rf69_initialize(stack[2], bandToFreq(stack[0]), stack[1],
     config.frequency_offset);
-    rf12_sendNow(stack[3], stack + 4, top - 4);
-    rf12_sendWait(2);
-    rf12_configSilent();
+    rf69_sendNow(stack[3], stack + 4, top - 4);
+    rf69_sendWait(2);
+    rf69_configSilent();
   }
   else if (c > ' ') {
     switch (c) {
@@ -382,9 +311,9 @@ static void handleInput (char c) {
 
     case 't': // broadcast a maximum size test packet, request an ack
       cmd = 'a';
-      sendLen = RF12_MAXDATA;
+      sendLen = RF69_MAXDATA;
       dest = 0;
-      for (byte i = 0; i < RF12_MAXDATA; ++i)
+      for (byte i = 0; i < RF69_MAXDATA; ++i)
         stack[i] = i + testCounter;
       showString(PSTR("test "));
       showByte(testCounter); // first byte in test buffer
@@ -398,29 +327,13 @@ static void handleInput (char c) {
       dest = value;
       break;
 
-    case 'f': // send FS20 command: <hchi>,<hclo>,<addr>,<cmd>f
-      rf12_initialize(0, RF12_868MHZ, 0);
-      activityLed(1);
-      fs20cmd(256 * stack[0] + stack[1], stack[2], value);
-      activityLed(0);
-      rf12_configSilent();
-      break;
-
-    case 'k': // send KAKU command: <addr>,<dev>,<on>k
-      rf12_initialize(0, RF12_433MHZ, 0);
-      activityLed(1);
-      kakuSend(stack[0], stack[1], value);
-      activityLed(0);
-      rf12_configSilent();
-      break;
-
     case 'z': // put the ATmega in ultra-low power mode (reset needed)
       if (value == 123) {
         showString(PSTR(" Zzz...\n"));
         Serial.flush();
-        rf12_sleep(RF12_SLEEP);
+        rf69_sleep(RF69_SLEEP);
         cli();
-        Sleepy::powerDown();
+        //Sleepy::powerDown();
       }
       break;
 
@@ -436,11 +349,12 @@ static void handleInput (char c) {
 
     case 'v': //display the interpreter version and configuration
       displayVersion();
-      rf12_configDump();
+      rf69_configDump();
       break;
 
     case 'l': // turn activity LED on or off
-      activityLed(value);
+      config.led_enable = value;
+      saveConfig();
       break;
 
     case 'd': // dump all log markers
@@ -470,11 +384,10 @@ static void handleInput (char c) {
         showString(PSTR("erased\n"));
       }
       break;
-#if RF69_COMPAT
+
     case 'y': // turn signal strength trace mode on or off (rfm69 only)
       trace_mode = value;
       break;
-#endif
 
     default:
       showHelp();
@@ -501,27 +414,26 @@ void setup () {
   
   activityLed(1);
   delay(1000); // shortened for now. Handy with JeeNode Micro V1 where ISP
-  activityLed(1);
-  // interaction can be upset by RF12B startup process.
+  // interaction can be upset by RF69B startup process.
 
   Serial.begin(SERIAL_BAUD);
   Serial.println();
   displayVersion();
 
-  if (rf12_configSilent()) {
+  if (rf69_configSilent()) {
     loadConfig();
   }
   else {
     memset(&config, 0, sizeof config);
-   config.nodeId = 0x4F;                           // RFM12Pi - 433 MHz, node 15
+    config.nodeId = 0x4F;                           // RFM12Pi - 433 MHz, node 15
     config.group = 0xD2;                           // RFM12Pi - default group 210
     config.frequency_offset = 1600;
     config.quiet_mode = true;   // Default flags, quiet on
     saveConfig();
-    rf12_configSilent();
+    rf69_configSilent();
   }
 
-  rf12_configDump();
+  rf69_configDump();
   df_initialize();
 
   delay(2000);        //rfm12pi keep LED for for 1s to show it's working at startup
@@ -531,15 +443,13 @@ void setup () {
 void loop () {
   if (Serial.available())
     handleInput(Serial.read());
-
-#if RF69_COMPAT
   if (trace_mode == 0) {
-#endif
-  if (rf12_recvDone()) {
-    byte n = rf12_len;
-    if (rf12_crc == 0)
+  if (rf69_recvDone()) {
+    byte n = rf69_len;
+    
+    if (rf69_crc == 0)
     {
-      activityLed(1);
+      if (config.led_enable) activityLed(1);
       showString(PSTR("OK"));
     }
     else {
@@ -553,24 +463,23 @@ void loop () {
       printOneChar('X');
     if (config.group == 0) {
       showString(PSTR(" G"));
-      showByte(rf12_grp);
+      showByte(rf69_grp);
     }
     printOneChar(' ');
-    showByte(rf12_hdr & 0x1F);
+    showByte(rf69_hdr & RF69_HDR_MASK);
     for (byte i = 0; i < n; ++i) {
       if (!config.hex_output)
         printOneChar(' ');
-      showByte(rf12_data[i]);
+      showByte(rf69_data[i]);
     }
-#if RF69_COMPAT
     // display RSSI value after packet data
     showString(PSTR(" ("));
     if (config.hex_output)
-      showByte(RF69::rssi);
+      showByte(rssi);
     else
-      Serial.print(-(RF69::rssi>>1));
+      Serial.print(-(rssi>>1));
     showString(PSTR(") "));
-#endif
+
     Serial.println();
 
     if (config.hex_output > 1) { // also print a line as ascii
@@ -578,44 +487,43 @@ void loop () {
       if (config.group == 0) {
         showString(PSTR(" II "));
       }
-      printOneChar(rf12_hdr & RF12_HDR_DST ? '>' : '<');
-      printOneChar('@' + (rf12_hdr & RF12_HDR_MASK));
-      displayASCII((const byte*) rf12_data, n);
+      printOneChar(rf69_hdr & RF69_HDR_DST ? '>' : '<');
+      printOneChar('@' + (rf69_hdr & RF69_HDR_MASK));
+      displayASCII((const byte*) rf69_data, n);
     }
 
-    if (rf12_crc == 0) {
-      activityLed(1);
+    if (rf69_crc == 0) {
+      if (config.led_enable) activityLed(1);
 
       if (df_present())
-        df_append((const char*) rf12_data - 2, rf12_len + 2);
+        df_append((const char*) rf69_data - 2, rf69_data + 2);
 
-      if (RF12_WANTS_ACK && (config.collect_mode) == 0) {
+      if (RF69_WANTS_ACK && (config.collect_mode) == 0) {
         showString(PSTR(" -> ack\n"));
-        rf12_sendStart(RF12_ACK_REPLY, 0, 0);
+        rf69_sendStart(RF69_ACK_REPLY, 0, 0);
       }
       activityLed(0);
     }
   }
 
-  if (cmd && rf12_canSend()) {
-    activityLed(1);
-
+  if (cmd && rf69_canSend()) {
+    if (config.led_enable) activityLed(1);
     showString(PSTR(" -> "));
     Serial.print((word) sendLen);
     showString(PSTR(" b\n"));
-    byte header = cmd == 'a' ? RF12_HDR_ACK : 0;
+    byte header = cmd == 'a' ? RF69_HDR_ACK : 0;
     if (dest)
-      header |= RF12_HDR_DST | dest;
-    rf12_sendStart(header, stack, sendLen);
+      header |= RF69_HDR_DST | dest;
+    rf69_sendStart(header, stack, sendLen);
     cmd = 0;
 
     activityLed(0);
   }
   activityLed(0);
-#if RF69_COMPAT
+
   } else {
-      rf12_recvDone();
-      byte y = (RF69::rssi>>1);
+      rf69_recvDone();
+      byte y = (rssi>>1);
       for (byte i = 0; i < (100-y); ++i) {
           printOneChar('-');
       }
@@ -625,9 +533,7 @@ void loop () {
       }
       Serial.print(-y);
       Serial.println("dB");
-
       delay(trace_mode*10);
 
   }
-#endif
 }
